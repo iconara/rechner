427fc46c53c46cace6205958b657b7e1297c5ba9
Theo <theo@iconara.net>
2017-01-26 23:01:00 +0100

    Wrap the Parser and Lexer tests in appropriate contexts

--- spec/rechner/lexer_spec.rb
+++ spec/rechner/lexer_spec.rb
@@ -4,165 +4,167 @@ module Rechner
       described_class
     end
 
-    it 'lexes a one-char number' do
-      expect(lexer.lex('1')).to eq([
-        described_class::NumberToken.new(1),
-        described_class::EndToken.new,
-      ])
-    end
+    describe '.lex' do
+      it 'lexes a one-char number' do
+        expect(lexer.lex('1')).to eq([
+          described_class::NumberToken.new(1),
+          described_class::EndToken.new,
+        ])
+      end
 
-    it 'lexes a multi-char number' do
-      expect(lexer.lex('123')).to eq([
-        described_class::NumberToken.new(123),
-        described_class::EndToken.new,
-      ])
-    end
+      it 'lexes a multi-char number' do
+        expect(lexer.lex('123')).to eq([
+          described_class::NumberToken.new(123),
+          described_class::EndToken.new,
+        ])
+      end
 
-    it 'lexes an addition' do
-      expect(lexer.lex('12+34')).to eq([
-        described_class::NumberToken.new(12),
-        described_class::PlusToken.new,
-        described_class::NumberToken.new(34),
-        described_class::EndToken.new,
-      ])
-    end
+      it 'lexes an addition' do
+        expect(lexer.lex('12+34')).to eq([
+          described_class::NumberToken.new(12),
+          described_class::PlusToken.new,
+          described_class::NumberToken.new(34),
+          described_class::EndToken.new,
+        ])
+      end
 
-    it 'ignores whitespace at the start of the input' do
-      expect(lexer.lex('  1')).to eq([
-        described_class::NumberToken.new(1),
-        described_class::EndToken.new,
-      ])
-    end
+      it 'ignores whitespace at the start of the input' do
+        expect(lexer.lex('  1')).to eq([
+          described_class::NumberToken.new(1),
+          described_class::EndToken.new,
+        ])
+      end
 
-    it 'ignores whitespace at the end of the input' do
-      expect(lexer.lex('  1')).to eq([
-        described_class::NumberToken.new(1),
-        described_class::EndToken.new,
-      ])
-    end
+      it 'ignores whitespace at the end of the input' do
+        expect(lexer.lex('  1')).to eq([
+          described_class::NumberToken.new(1),
+          described_class::EndToken.new,
+        ])
+      end
 
-    it 'ignores whitespace between operands and operators' do
-      expect(lexer.lex('1 + 1')).to eq([
-        described_class::NumberToken.new(1),
-        described_class::PlusToken.new,
-        described_class::NumberToken.new(1),
-        described_class::EndToken.new,
-      ])
-    end
+      it 'ignores whitespace between operands and operators' do
+        expect(lexer.lex('1 + 1')).to eq([
+          described_class::NumberToken.new(1),
+          described_class::PlusToken.new,
+          described_class::NumberToken.new(1),
+          described_class::EndToken.new,
+        ])
+      end
 
-    it 'lexes a one-char identifier' do
-      expect(lexer.lex('a')).to eq([
-        described_class::IdentifierToken.new('a'),
-        described_class::EndToken.new,
-      ])
-    end
+      it 'lexes a one-char identifier' do
+        expect(lexer.lex('a')).to eq([
+          described_class::IdentifierToken.new('a'),
+          described_class::EndToken.new,
+        ])
+      end
 
-    it 'lexes a multi-char identifier' do
-      expect(lexer.lex('abc')).to eq([
-        described_class::IdentifierToken.new('abc'),
-        described_class::EndToken.new,
-      ])
-    end
+      it 'lexes a multi-char identifier' do
+        expect(lexer.lex('abc')).to eq([
+          described_class::IdentifierToken.new('abc'),
+          described_class::EndToken.new,
+        ])
+      end
 
-    it 'lexes an identifier with an underscore' do
-      expect(lexer.lex('ab_c')).to eq([
-        described_class::IdentifierToken.new('ab_c'),
-        described_class::EndToken.new,
-      ])
-    end
+      it 'lexes an identifier with an underscore' do
+        expect(lexer.lex('ab_c')).to eq([
+          described_class::IdentifierToken.new('ab_c'),
+          described_class::EndToken.new,
+        ])
+      end
 
-    it 'lexes an identifier with numbers' do
-      expect(lexer.lex('abc123')).to eq([
-        described_class::IdentifierToken.new('abc123'),
-        described_class::EndToken.new,
-      ])
-    end
+      it 'lexes an identifier with numbers' do
+        expect(lexer.lex('abc123')).to eq([
+          described_class::IdentifierToken.new('abc123'),
+          described_class::EndToken.new,
+        ])
+      end
 
-    it 'lexes an addition with identifiers' do
-      expect(lexer.lex('abc + def')).to eq([
-        described_class::IdentifierToken.new('abc'),
-        described_class::PlusToken.new,
-        described_class::IdentifierToken.new('def'),
-        described_class::EndToken.new,
-      ])
-    end
+      it 'lexes an addition with identifiers' do
+        expect(lexer.lex('abc + def')).to eq([
+          described_class::IdentifierToken.new('abc'),
+          described_class::PlusToken.new,
+          described_class::IdentifierToken.new('def'),
+          described_class::EndToken.new,
+        ])
+      end
 
-    it 'lexes an addition with identifiers and numbers' do
-      expect(lexer.lex('abc + def + 123')).to eq([
-        described_class::IdentifierToken.new('abc'),
-        described_class::PlusToken.new,
-        described_class::IdentifierToken.new('def'),
-        described_class::PlusToken.new,
-        described_class::NumberToken.new(123),
-        described_class::EndToken.new,
-      ])
-    end
+      it 'lexes an addition with identifiers and numbers' do
+        expect(lexer.lex('abc + def + 123')).to eq([
+          described_class::IdentifierToken.new('abc'),
+          described_class::PlusToken.new,
+          described_class::IdentifierToken.new('def'),
+          described_class::PlusToken.new,
+          described_class::NumberToken.new(123),
+          described_class::EndToken.new,
+        ])
+      end
 
-    it 'lexes a subtraction' do
-      expect(lexer.lex('a - 1')).to eq([
-        described_class::IdentifierToken.new('a'),
-        described_class::MinusToken.new,
-        described_class::NumberToken.new(1),
-        described_class::EndToken.new,
-      ])
-    end
+      it 'lexes a subtraction' do
+        expect(lexer.lex('a - 1')).to eq([
+          described_class::IdentifierToken.new('a'),
+          described_class::MinusToken.new,
+          described_class::NumberToken.new(1),
+          described_class::EndToken.new,
+        ])
+      end
 
-    it 'lexes a multiplication' do
-      expect(lexer.lex('a * 1')).to eq([
-        described_class::IdentifierToken.new('a'),
-        described_class::MultiplicationToken.new,
-        described_class::NumberToken.new(1),
-        described_class::EndToken.new,
-      ])
-    end
+      it 'lexes a multiplication' do
+        expect(lexer.lex('a * 1')).to eq([
+          described_class::IdentifierToken.new('a'),
+          described_class::MultiplicationToken.new,
+          described_class::NumberToken.new(1),
+          described_class::EndToken.new,
+        ])
+      end
 
-    it 'lexes a division' do
-      expect(lexer.lex('a / 1')).to eq([
-        described_class::IdentifierToken.new('a'),
-        described_class::DivisionToken.new,
-        described_class::NumberToken.new(1),
-        described_class::EndToken.new,
-      ])
-    end
+      it 'lexes a division' do
+        expect(lexer.lex('a / 1')).to eq([
+          described_class::IdentifierToken.new('a'),
+          described_class::DivisionToken.new,
+          described_class::NumberToken.new(1),
+          described_class::EndToken.new,
+        ])
+      end
 
-    it 'lexes unary minus' do
-      expect(lexer.lex('-a')).to eq([
-        described_class::MinusToken.new,
-        described_class::IdentifierToken.new('a'),
-        described_class::EndToken.new,
-      ])
-    end
+      it 'lexes unary minus' do
+        expect(lexer.lex('-a')).to eq([
+          described_class::MinusToken.new,
+          described_class::IdentifierToken.new('a'),
+          described_class::EndToken.new,
+        ])
+      end
 
-    it 'lexes parentheses' do
-      expect(lexer.lex('a + 3 * (c - d)')).to eq([
-        described_class::IdentifierToken.new('a'),
-        described_class::PlusToken.new,
-        described_class::NumberToken.new(3),
-        described_class::MultiplicationToken.new,
-        described_class::OpenParenthesisToken.new,
-        described_class::IdentifierToken.new('c'),
-        described_class::MinusToken.new,
-        described_class::IdentifierToken.new('d'),
-        described_class::CloseParenthesisToken.new,
-        described_class::EndToken.new,
-      ])
-    end
+      it 'lexes parentheses' do
+        expect(lexer.lex('a + 3 * (c - d)')).to eq([
+          described_class::IdentifierToken.new('a'),
+          described_class::PlusToken.new,
+          described_class::NumberToken.new(3),
+          described_class::MultiplicationToken.new,
+          described_class::OpenParenthesisToken.new,
+          described_class::IdentifierToken.new('c'),
+          described_class::MinusToken.new,
+          described_class::IdentifierToken.new('d'),
+          described_class::CloseParenthesisToken.new,
+          described_class::EndToken.new,
+        ])
+      end
 
-    it 'raises an error when the input contains unexpected characters' do
-      expect { lexer.lex('1 # 1') }.to raise_error(LexerError, 'Unexpected input "#" at position 3')
-    end
+      it 'raises an error when the input contains unexpected characters' do
+        expect { lexer.lex('1 # 1') }.to raise_error(LexerError, 'Unexpected input "#" at position 3')
+      end
 
-    it 'raises an error when a string that starts with a number continues with letters', pending: 'numbers will have to be validated properly' do
-      expect { lexer.lex('1a') }.to raise_error(LexerError, 'Unexpected input "a" at position 2')
-    end
+      it 'raises an error when a string that starts with a number continues with letters', pending: 'numbers will have to be validated properly' do
+        expect { lexer.lex('1a') }.to raise_error(LexerError, 'Unexpected input "a" at position 2')
+      end
 
-    it 'lexes ungrammatical strings' do
-      expect(lexer.lex('1 1 +')).to eq([
-        described_class::NumberToken.new(1),
-        described_class::NumberToken.new(1),
-        described_class::PlusToken.new,
-        described_class::EndToken.new,
-      ])
+      it 'lexes ungrammatical strings' do
+        expect(lexer.lex('1 1 +')).to eq([
+          described_class::NumberToken.new(1),
+          described_class::NumberToken.new(1),
+          described_class::PlusToken.new,
+          described_class::EndToken.new,
+        ])
+      end
     end
   end
 end
--- spec/rechner/parser_spec.rb
+++ spec/rechner/parser_spec.rb
@@ -4,146 +4,148 @@ module Rechner
       described_class
     end
 
-    it 'parses a constant' do
-      expect(parser.parse('1')).to eq(
-        described_class::ConstantExpression.new(1)
-      )
-    end
-
-    it 'parses an addition of constants' do
-      expect(parser.parse('1 + 2')).to eq(
-        described_class::AdditionExpression.new(
-          described_class::ConstantExpression.new(1),
-          described_class::ConstantExpression.new(2)
+    describe '.parse' do
+      it 'parses a constant' do
+        expect(parser.parse('1')).to eq(
+          described_class::ConstantExpression.new(1)
         )
-      )
-    end
+      end
 
-    it 'parses a reference' do
-      expect(parser.parse('a')).to eq(
-        described_class::ReferenceExpression.new('a')
-      )
-    end
-
-    it 'parses an addition of references' do
-      expect(parser.parse('a + b')).to eq(
-        described_class::AdditionExpression.new(
-          described_class::ReferenceExpression.new('a'),
-          described_class::ReferenceExpression.new('b')
-        )
-      )
-    end
-
-    it 'parses an string of additions' do
-      expect(parser.parse('1 + a + 2 + b')).to eq(
-        described_class::AdditionExpression.new(
-          described_class::ConstantExpression.new(1),
-          described_class::AdditionExpression.new(
-            described_class::ReferenceExpression.new('a'),
-            described_class::AdditionExpression.new(
-              described_class::ConstantExpression.new(2),
-              described_class::ReferenceExpression.new('b')
-            )
-          )
-        )
-      )
-    end
-
-    it 'parses a multiplication' do
-      expect(parser.parse('1 * a')).to eq(
-        described_class::MultiplicationExpression.new(
-          described_class::ConstantExpression.new(1),
-          described_class::ReferenceExpression.new('a')
-        )
-      )
-    end
-
-    it 'parses mixed addition and multiplication with multiplication having higher precedence' do
-      expect(parser.parse('1 + a * b + 2')).to eq(
-        described_class::AdditionExpression.new(
-          described_class::ConstantExpression.new(1),
-          described_class::AdditionExpression.new(
-            described_class::MultiplicationExpression.new(
-              described_class::ReferenceExpression.new('a'),
-              described_class::ReferenceExpression.new('b')
-            ),
-            described_class::ConstantExpression.new(2)
-          )
-        )
-      )
-    end
-
-    it 'parses mixed subtraction and division with division having higher precedence' do
-      expect(parser.parse('1 - a / b - 2')).to eq(
-        described_class::SubtractionExpression.new(
-          described_class::ConstantExpression.new(1),
-          described_class::SubtractionExpression.new(
-            described_class::DivisionExpression.new(
-              described_class::ReferenceExpression.new('a'),
-              described_class::ReferenceExpression.new('b')
-            ),
-            described_class::ConstantExpression.new(2)
-          )
-        )
-      )
-    end
-
-    it 'parses parenthesized expressions' do
-      expect(parser.parse('(1 + 2) * 3')).to eq(
-        described_class::MultiplicationExpression.new(
+      it 'parses an addition of constants' do
+        expect(parser.parse('1 + 2')).to eq(
           described_class::AdditionExpression.new(
             described_class::ConstantExpression.new(1),
             described_class::ConstantExpression.new(2)
-          ),
-          described_class::ConstantExpression.new(3)
+          )
         )
-      )
-    end
-
-    context 'returns a compiled expression that' do
-      it 'can describe itself' do
-        expect(parser.parse('1 + (2 * (a - b) - c * 5)').to_s).to eq('(1 + ((2 * (a - b)) - (c * 5)))')
-        expect(parser.parse(parser.parse('1 + (2 * (a - b) - c * 5)').to_s).to_s).to eq('(1 + ((2 * (a - b)) - (c * 5)))')
       end
 
-      it 'can calculate its own value' do
-        expect(parser.parse('1 + 2 + 3 + 4').calculate).to eq(1 + 2 + 3 + 4)
+      it 'parses a reference' do
+        expect(parser.parse('a')).to eq(
+          described_class::ReferenceExpression.new('a')
+        )
       end
 
-      context 'when containing references' do
-        it 'can return those references\' names' do
-          expect(parser.parse('a + b * c + d + 4').references).to eq([:a, :b, :c, :d])
+      it 'parses an addition of references' do
+        expect(parser.parse('a + b')).to eq(
+          described_class::AdditionExpression.new(
+            described_class::ReferenceExpression.new('a'),
+            described_class::ReferenceExpression.new('b')
+          )
+        )
+      end
+
+      it 'parses an string of additions' do
+        expect(parser.parse('1 + a + 2 + b')).to eq(
+          described_class::AdditionExpression.new(
+            described_class::ConstantExpression.new(1),
+            described_class::AdditionExpression.new(
+              described_class::ReferenceExpression.new('a'),
+              described_class::AdditionExpression.new(
+                described_class::ConstantExpression.new(2),
+                described_class::ReferenceExpression.new('b')
+              )
+            )
+          )
+        )
+      end
+
+      it 'parses a multiplication' do
+        expect(parser.parse('1 * a')).to eq(
+          described_class::MultiplicationExpression.new(
+            described_class::ConstantExpression.new(1),
+            described_class::ReferenceExpression.new('a')
+          )
+        )
+      end
+
+      it 'parses mixed addition and multiplication with multiplication having higher precedence' do
+        expect(parser.parse('1 + a * b + 2')).to eq(
+          described_class::AdditionExpression.new(
+            described_class::ConstantExpression.new(1),
+            described_class::AdditionExpression.new(
+              described_class::MultiplicationExpression.new(
+                described_class::ReferenceExpression.new('a'),
+                described_class::ReferenceExpression.new('b')
+              ),
+              described_class::ConstantExpression.new(2)
+            )
+          )
+        )
+      end
+
+      it 'parses mixed subtraction and division with division having higher precedence' do
+        expect(parser.parse('1 - a / b - 2')).to eq(
+          described_class::SubtractionExpression.new(
+            described_class::ConstantExpression.new(1),
+            described_class::SubtractionExpression.new(
+              described_class::DivisionExpression.new(
+                described_class::ReferenceExpression.new('a'),
+                described_class::ReferenceExpression.new('b')
+              ),
+              described_class::ConstantExpression.new(2)
+            )
+          )
+        )
+      end
+
+      it 'parses parenthesized expressions' do
+        expect(parser.parse('(1 + 2) * 3')).to eq(
+          described_class::MultiplicationExpression.new(
+            described_class::AdditionExpression.new(
+              described_class::ConstantExpression.new(1),
+              described_class::ConstantExpression.new(2)
+            ),
+            described_class::ConstantExpression.new(3)
+          )
+        )
+      end
+
+      context 'returns a compiled expression that' do
+        it 'can describe itself' do
+          expect(parser.parse('1 + (2 * (a - b) - c * 5)').to_s).to eq('(1 + ((2 * (a - b)) - (c * 5)))')
+          expect(parser.parse(parser.parse('1 + (2 * (a - b) - c * 5)').to_s).to_s).to eq('(1 + ((2 * (a - b)) - (c * 5)))')
         end
 
-        context 'and some references occur more than once' do
-          it 'can return the unique references' do
-            expect(parser.parse('a + a + a * b').references).to eq([:a, :b])
-          end  
+        it 'can calculate its own value' do
+          expect(parser.parse('1 + 2 + 3 + 4').calculate).to eq(1 + 2 + 3 + 4)
         end
 
-        context 'and is given bindings' do
-          it 'can calculate its own value' do
-            expect(parser.parse('a + b + 3 + 4').calculate(a: 1, b: 2)).to eq(1 + 2 + 3 + 4)
+        context 'when containing references' do
+          it 'can return those references\' names' do
+            expect(parser.parse('a + b * c + d + 4').references).to eq([:a, :b, :c, :d])
           end
 
-          context 'but not all references are present in the bindings' do
-            it 'raises an error' do
-              expect { parser.parse('a + b').calculate(a: 1) }.to raise_error(ReferenceError, 'No binding for "b"')
+          context 'and some references occur more than once' do
+            it 'can return the unique references' do
+              expect(parser.parse('a + a + a * b').references).to eq([:a, :b])
+            end  
+          end
+
+          context 'and is given bindings' do
+            it 'can calculate its own value' do
+              expect(parser.parse('a + b + 3 + 4').calculate(a: 1, b: 2)).to eq(1 + 2 + 3 + 4)
+            end
+
+            context 'but not all references are present in the bindings' do
+              it 'raises an error' do
+                expect { parser.parse('a + b').calculate(a: 1) }.to raise_error(ReferenceError, 'No binding for "b"')
+              end
             end
           end
         end
       end
-    end
 
-    context 'when there are trailing tokens' do
-      it 'raises an error' do
-        expect { parser.parse('a + b a') }.to raise_error(ParseError, 'Trailing tokens after expression')
+      context 'when there are trailing tokens' do
+        it 'raises an error' do
+          expect { parser.parse('a + b a') }.to raise_error(ParseError, 'Trailing tokens after expression')
+        end
       end
-    end
 
-    context 'when a closing parenthesis is missing' do
-      it 'raises an error' do
-        expect { parser.parse('(a + (b * c)') }.to raise_error(ParseError, 'Missing closing parenthesis')
+      context 'when a closing parenthesis is missing' do
+        it 'raises an error' do
+          expect { parser.parse('(a + (b * c)') }.to raise_error(ParseError, 'Missing closing parenthesis')
+        end
       end
     end
   end
